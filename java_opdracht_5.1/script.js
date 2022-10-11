fcda417a8aa058645bb2e41e79d5c9471f116456
TuncaySteinmann <112862509+TuncaySteinmann@users.noreply.github.com>
2022-10-11 13:58:47 +0200

    Add files via upload

---
+++ java_opdracht_5.1/script.js
@@ -0,0 +1,13 @@
+alert('hallo wereld')
+
+document.getElementById("title").style.color = "red";
+
+var getal1 = 2;
+var getal2  = 4;
+var som =  getal1  * getal2;
+alert(getal1 + "X"  + getal2 + "=" + som)
+
+var kleur = prompt("blue", "");
+
+document.getElementById("title").style.color = kleur;
+document.getElementById("p1").innerHTML = "hello" + "!";
