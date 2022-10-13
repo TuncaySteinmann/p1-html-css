2f8f4820f35d101eda9573eb5f17d173edf4c3d0
TuncaySteinmann <112862509+TuncaySteinmann@users.noreply.github.com>
2022-10-13 11:47:52 +0200

    Add files via upload

---
+++ java_opdracht_6.4/script.js
@@ -0,0 +1,17 @@
+function Calculator(num,num2){
+    let answer = num + num2;
+    console.log(answer);
+    alert(answer);
+}
+
+function multiplyByFive(num,num2){
+    let answer = num * num2;
+    console.log(answer);
+    alert(answer);
+}
+
+function calculateMinutes(num,num2){
+    let answer = num / num2;
+    console.log(answer);
+    alert(answer);
+}
