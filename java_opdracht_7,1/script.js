5ab26245344dffba1b7aed46c6a1b037fa1b48ea
TuncaySteinmann <112862509+TuncaySteinmann@users.noreply.github.com>
2022-10-17 10:53:31 +0200

    Add files via upload

---
+++ java_opdracht_7,1/script.js
@@ -0,0 +1,17 @@
+var budget = 100;
+
+var product = 60;
+
+
+function opdracht71() {
+  if(budget > product) {
+    alert("u heeft genoeg geld!")
+    alert("Fijne dag")
+  }
+
+  else if(budget < product){
+    alert("U heeft niet genoeg geld")
+    alert("fijne dag")
+  }
+}
+
